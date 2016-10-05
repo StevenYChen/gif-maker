7e2849d9cda46afa7fd07cafe6a62fd52dbe7ebc
Steven Chen <Chen.steven.yu@gmail.com>
2016-10-05 15:40:57 -0500

    Feat: Add empty UIViewcontroller

---
+++ GifMaker_Swift_Template/UIViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by Steven Chen on 10/5/16.
+//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
