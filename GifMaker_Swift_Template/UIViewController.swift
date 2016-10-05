a902539046ad94a8b24eb66821e11187a1146f50
Steven Chen <Chen.steven.yu@gmail.com>
2016-10-05 16:21:27 -0500

    Feat: Add Camera
    - Allows camera to record
    - Saves recorded video to photo album

--- GifMaker_Swift_Template/UIViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIViewController.swift
-//  GifMaker_Swift_Template
-//
-//  Created by Steven Chen on 10/5/16.
-//  Copyright © 2016 Gabrielle Miller-Messner. All rights reserved.
-//
-
-import Foundation
